8bcc1b2f90aab45ddf968d3564626c2e28a83254
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-07 08:50:57 +0300

    ft:added the admin config

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post,Followers,Comment,Profile
 
 # Register your models here.
+# admin.site.register(Post)
+admin.site.register(Followers)
+admin.site.register(Comment)
+admin.site.register(Profile)
+admin.site.register(Post)
