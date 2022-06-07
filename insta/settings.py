c62be748ec2771cdc195237f0550aeac549944b6
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-07 12:47:14 +0300

    deployment to heroku

--- insta/settings.py
+++ insta/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 MODE='prod'
 
 ALLOWED_HOSTS = ['.localhost', 'boninsta221.herokuapp.com', '.127.0.0.1']
