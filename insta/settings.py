221f95cd4ea353918a25c4a4727be862a4acb8c5
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-07 14:05:00 +0300

    deployment to heroku

--- insta/settings.py
+++ insta/settings.py
@@ -130,10 +130,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
     
-)
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
