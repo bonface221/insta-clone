40c0b0d806e716bcf74a118f38e4afec6cb8f08b
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-08 20:42:11 +0300

    bug fix

--- insta/settings.py
+++ insta/settings.py
@@ -18,8 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-MODE='prod'
+DEBUG = True
+MODE='dev'
 
 ALLOWED_HOSTS = ['.localhost', '.herokuapp.com', '127.0.0.1']
 
